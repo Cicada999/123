049e8368fc7333fe00fba9f61ed1164698cd2dee
Cicada999 <ap190933@gmail.com>
2024-11-10 15:05:15 +0200

    Update new.py

--- new.py
+++ new.py
@@ -29,7 +29,18 @@ cicada_kb.add(
     InlineKeyboardButton('❓ Сколько Ботов ?', callback_data='cislo')
 )
 # Настройки бота
+# Соединение с Redis
+redis = None
+async def init_redis():
+    global redis
+    redis = await aioredis.create_redis_pool(REDIS_URL)
 
+# Соединение с PostgreSQL
+db_pool = None
+async def init_db():
+    global db_pool
+    db_pool = await asyncpg.create_pool(DATABASE_URL)
+    
 bot = Bot(token=token, parse_mode="HTML")
 dp = Dispatcher(bot, storage=MemoryStorage())
 
@@ -120,52 +131,84 @@ async def input_text_for_ad(message: types.Message, state: FSMContext):
 
 # Получение активного бота
 async def get_active_bot():
-    conn = connect_to_db()
-    cursor = conn.cursor()
-    cursor.execute("SELECT username FROM bots WHERE is_active = TRUE;")
-    bots = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    if bots:
-        return random.choice(bots)[0]
-    return None
+    async with db_pool.acquire() as conn:
+        bot_usernames = await conn.fetch("SELECT username FROM bots WHERE is_active = TRUE")
+        bot_usernames = [record['username'] for record in bot_usernames]
+        if not bot_usernames:
+            return None
+        return random.choice(bot_usernames)
 
-# Обработчик получения бота
-async def nowi(message: types.Message):
-    chat_id = message.chat.id
-    while True:
-        bot_username = await get_active_bot()
-        if not bot_username:
-            await message.answer("<b>Нет доступных ботов в данный момент.</b>", reply_markup=menu)
-            return
+# Обновление информации о ботах в базе данных
+async def update_bot_status(bot_username, is_active):
+    async with db_pool.acquire() as conn:
+        await conn.execute("UPDATE bots SET is_active = $1 WHERE username = $2", is_active, bot_username)
 
-        if await is_bot_alive(bot_username):
-            redis_client.set(f"user:{chat_id}:bot", bot_username)
-            await message.answer(
-                f"<b>✳️ Привет {message.from_user.first_name} ✳️</b>\n"
-                f"გამარჯობა {message.from_user.first_name}\n\n"
-                f"➖➖➖➖➖➖➖➖➖➖➖\n"
-                f"<b>Твой Бот: <a href='http://t.me/{bot_username}'>@{bot_username}</a> Жив</b>\n"
-                f"შენი ბოტი: <a href='http://t.me/{bot_username}'>@{bot_username}</a> ცოცხალია\n"
-                f"➖➖➖➖➖➖➖➖➖➖➖\n"
-                f"<b>Если Тот Умрет Вернись Сюда И Получишь Новый:</b>\n"
-                f"თუ ის მოკვდება, დაბრუნდი აქ და მიიღეთ ახალი",
-                reply_markup=menu
-            )
-            break
-        else:
-            remove_bot_from_db(bot_username)
+# Функция для выдачи нового бота
+async def nowi(message):
+    chat_id = str(message.chat.id)
+    bot_username = await get_active_bot()
 
-# Обработчик команды /start
-@dp.message_handler(commands=['start'], state="*")
-async def show_contact(message: types.Message, state: FSMContext):
-    chat_id = message.chat.id
-    if redis_client.get(f"user:{chat_id}:bot"):
-        await message.answer("<b>Уже назначен бот.</b>")
+    if not bot_username:
+        await message.answer("<b>Боты скоро появятся</b>", reply_markup=menu)
+        return
+
+    # Проверка доступности бота
+    if await is_bot_alive(bot_username):
+        # Сохранение в Redis
+        await redis.set(chat_id, bot_username)
+        
+        # Сообщение о выдаче бота
+        sss = await message.answer(
+            f"<b>✳️ Привет {message.from_user.first_name} ✳️</b>\n"
+            f"გამარჯობა {message.from_user.first_name}\n\n"
+            f"➖➖➖➖➖➖➖➖➖➖➖\n"
+            f"<b>Твой Бот: <a href='http://t.me/{bot_username}'>@{bot_username}</a> Жив</b>\n"
+            f"შენი ბოტი: <a href='http://t.me/{bot_username}'>@{bot_username}</a> ცოცხალია\n"
+            f"➖➖➖➖➖➖➖➖➖➖➖\n"
+            f"<b>Если Тот Умрет Вернись Сюда И Получишь Новый:</b>\n"
+            f"თუ ის მოკვდება, დაბრუნდი აქ და მიიღეთ ახალი",
+            reply_markup=menu
+        )
+        await bot.pin_chat_message(chat_id=message.chat.id, message_id=sss.message_id)
+    else:
+        # Обновление статуса бота на неактивный
+        await update_bot_status(bot_username, False)
+        await nowi(message)
+
+# Функция для повторного получения бота
+async def starii(message):
+    chat_id = str(message.chat.id)
+    bot_username = await redis.get(chat_id)
+    
+    if bot_username and await is_bot_alive(bot_username.decode()):
+        await message.answer(
+            f"<b>✳️ Привет {message.from_user.first_name} ✳️</b>\n"
+            f"გამარჯობა {message.from_user.first_name}\n\n"
+            f"➖➖➖➖➖➖➖➖➖➖➖\n"
+            f"<b>Твой Бот: <a href='http://t.me/{bot_username.decode()}'>@{bot_username.decode()}</a> Жив</b>\n"
+            f"შენი ბოტი: <a href='http://t.me/{bot_username.decode()}'>@{bot_username.decode()}</a> ცოცხალია\n"
+            f"➖➖➖➖➖➖➖➖➖➖➖\n"
+            f"<b>Если Тот Умрет Вернись Сюда И Получишь Новый:</b>\n"
+            f"თუ ის მოკვდება, დაბრუნდი აქ და მიიღეთ ახალი",
+            reply_markup=menu
+        )
     else:
         await nowi(message)
 
-if __name__ == '__main__':
-    from aiogram import executor
-    executor.start_polling(dp, skip_updates=True)
+# Обработчик команды /start и нажатия кнопки "ℹ️ Получить Бота ℹ️"
+@dp.message_handler(commands=["start"])
+@dp.message_handler(lambda message: message.text == "ℹ️ Получить Бота ℹ️")
+async def show_contact(message: types.Message, state: FSMContext):
+    chat_id = str(message.chat.id)
+    if await redis.exists(chat_id):
+        await starii(message)
+    else:
+        await nowi(message)
 
+# Инициализация
+async def on_startup(dp):
+    await init_redis()
+    await init_db()
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
